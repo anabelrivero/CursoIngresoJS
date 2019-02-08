281e780f1ce342783fb2282d6ed99148e60efcc3
anabelrivero <47437124+anabelrivero@users.noreply.github.com>
2019-02-07 21:56:34 -0300

    primer commit

---
+++ jsEntradaSalida-3.js
@@ -0,0 +1,14 @@
+/*Debemos lograr tomar un dato por 'ID'
+y luego mostrarlo por 'Alert' al presionar el botón  'mostrar'*/
+function mostrar()
+{
+	//elNombre.value="Pita Sanchez";
+	//alert (elNombre)
+	var nombre;
+	//nombre = prompt("Ingrese su nombre y apellido", "Pita Sanchez");
+	nombre=elNombre.value;
+	alert("tu nombre dice: " + nombre);
+
+}
+
+
---
+++ jsEntradaSalida-4.js
@@ -0,0 +1,13 @@
+/* 
+	Debemos lograr tomar un dato por 'PROMPT' 
+	y lo muestro por 'getElementById' al presionar el botón 'mostrar'
+*/
+function mostrar()
+{
+
+	var nombre =
+	 nombre = prompt("Ingrese su nombre y apellido");
+	 document.getElementsBy('elNombre').value=nombre;
+}
+
+
