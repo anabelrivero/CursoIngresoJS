94c1f22b5a15b730e04a17be9718db4774061df2
anabelrivero <47437124+anabelrivero@users.noreply.github.com>
2019-02-10 00:02:38 -0300

    git commit
    ejercicio 10

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,5 +4,17 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
-	
+	var aumento;
+	var descuento;
+
+	aumento=importe.value;
+
+	var aumento=parseInt(aumento);
+	var descuento=parseInt(descuento);
+
+	descuento=aumento*75/100;
+
+	resultado.value=descuento;
+
+
 }
