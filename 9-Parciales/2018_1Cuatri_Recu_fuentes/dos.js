7d7fd2d1384424f048b2341d99f0eb2462f90692
alumno <alumno>
2019-02-14 20:18:43 -0300

    ejercicios

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/dos.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/dos.js
@@ -1,4 +1,14 @@
 function mostrar()
 {
-  
+	var compra;
+	var descuento;
+	var masIva;
+
+	compra=prompt("Ingrese el valor de su compra");
+
+	descuento=compra-(compra*10/100);
+	masIva=compra+(compra*21/100);
+
+	alert("tu compra es de "+compra+" tienes un descuento del 10% queda en "+descuento+" mas el iva que es "+masIva);
+
 }
--- 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
@@ -1,5 +1,32 @@
 
 function mostrar()
 {
+	var primerPeso;
+	var segundoPeso;
+	var mascotaUno;
+	var mascotaDos;
+	var suma;
+
+	var primerPeso=prompt("Ingresar el peso");
+	var segundoPeso=prompt("Ingresar el peso");
+	var mascotaUno=prompt("Ingresar nombre");
+	var mascotaDos=prompt("Ingresar nombre");
+
+	var primerPeso=parseint(primerPeso);
+	var segundoPeso=parseint(segundoPeso):
+
+
+	suma=primerPeso+segundoPeso;
+	
+	alert("tienes dos mascotas "+mascotaUno+" y "+mascotaDos+" que pesan "+primerPeso+" y "+segundoPeso+ " kilos, y la suma de los kilos es"+suma);
+		}
+
+
+
+
+
+
+
+
 
 }
