bf5413a05012d96c5d4fcfc5d299f39292fbbadf
alumno <alumno>
2019-02-21 21:19:01 -0300

    ejercicio 5

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/cinco.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/cinco.js
@@ -1,4 +1,39 @@
 function mostrar()
 {
+	var habitacion;
+	var descuento;
+	var tipoPago;
+	var paquetes;
+	var descuento;
+	var precioHabitacion;
+	var precioTotal;
 
+	descuento=100;
+
+	habitacion=prompt("¿Que tipo de habitacion?");
+	precioHabitacion=prompt("Ingrese precio de la habitacion");
+	tipoPago=prompt("¿como desea pagar?");
+	paquetes=prompt("¿que tipo de paquete desea?");
+
+
+	switch (tipoPago)
+	{
+		case "visa":
+		case "mercadoPago":
+		descuento=90;
+		break;
+		case "paypal":
+		descuento=85;
+		break;
+		case "efectivo":
+		descuento=80;
+		break;
+		case "otros":
+		descuento=95;
+
+		break;	
+	}
+	
+	precioTotal=tipoPago+paquetes;
+	alert(precioTotal);
 }
