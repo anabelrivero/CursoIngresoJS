8aefaa4380a7ca3089f2478cceed921bb6915bd0
Your Name <anabela9619@gmai.com>
2019-02-26 12:12:10 -0300

    Parciales en casa

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
@@ -1,4 +1,13 @@
 function mostrar()
 {
+	var precio;
+	var descuento;
+	var precioFinal;
 
+	precio=prompt("Dame el precio");
+	descuento=prompt("dame el descuento");
+
+	precioFinal=precio-(precio*descuento/100);
+
+	elPrecioFinal.value=precioFinal;
 }
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/uno.js
@@ -1,5 +1,20 @@
 
 function mostrar()
 {
+	var ancho;
+	var largo;
+	var perimetro;
+
+	ancho=prompt("Dame el ancho");
+	largo=prompt("Dame el largo");
+
+	ancho=parseInt(ancho);
+	largo=parseInt(largo);
+
+	perimetro=(ancho*2)+(largo*2);
+
+	alert("El ancho es "+ancho+" y el largo es "+largo+" entonces tu perimetro es "+perimetro);
+
+
 
 }
