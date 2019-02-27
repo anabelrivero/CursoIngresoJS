52a61b624838e5e99746aaa030f078af9413d17d
Your Name <anabela9619@gmai.com>
2019-02-27 08:52:12 -0300

    parciales en casa 2

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
@@ -1,4 +1,32 @@
 function mostrar()
 {
+	var numeroUno;
+	var numeroDos;
+	var resultado;
+	var mensaje;
 
+	numeroUno=prompt("Ingrese un numero");
+	numeroDos=prompt("Ingrese otro numero");
+
+	numeroUno=parseInt(numeroUno);
+	numeroDos=parseInt(numeroDos);
+
+	if(numeroUno==numeroDos)
+	{
+		mensaje=("los numeros son iguales"+numeroUno+" "+numeroDos)
+	}else 
+	{
+		if(numeroUno>numeroDos)
+		{
+			resultado=numeroUno-numeroDos;
+		}else
+		{	
+			resultado=numeroUno+numeroDos;
+		}
+	} if(resultado>10)
+	{
+		mensaje=("la suma es "+resultado+" y supero el 10");
+	}
+
+alert(resultado+mensaje);
 }
